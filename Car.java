da1423882227a4777f9a985ca8b9bedaa0368826
Maria <123293512+MariaDtr@users.noreply.github.com>
2023-10-03 12:16:34 +0300

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,34 @@
+package Car;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int horsePower;
+
+    public String getBrand() {return brand;}
+
+    public String getModel() {return model;}
+
+    public int getHorsePower() {return horsePower;}
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String carInfo (){
+        return toString();
+    }
+    public String toString () {
+        return String.format("The car is: %s %s - %d HP.", this.brand, this.model, this.horsePower);
+    }
+
+}
+
